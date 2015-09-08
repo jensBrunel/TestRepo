118f3553eadf864ab3dc1e5128ba20e73ec2a4f9
jensBrunel <jens.nelke@web.de>
2015-09-08 16:03:25 +0200

    Added boost library

--- main.cpp
+++ main.cpp
@@ -2,29 +2,58 @@
   using std::cerr;
   using std::cout;
   using std::endl;
+#include <string>
+  using std::string;
+
+#include <boost/program_options.hpp>
+  namespace po = boost::program_options;
 
 #include "ToDoCore/ToDo.h"
   using ToDoCore::ToDo;
 
-#define EXPECT_EQUAL(test, expect) equalityTest( test,  expect, \
-                                                #test, #expect, \
-                                                __FILE__, __LINE__)
-
-template < typename T1, typename T2 >
-int equalityTest(const T1    testValue,
-                 const T2    expectedValue,
-                 const char* testName,
-                 const char* expectedName,
-                 const char* fileName,
-                 const int   lineNumber);
-
-
 int main(
-    int,
-    char**
+    int    argc,
+    char** argv
 )
 {
-    int result = 0;
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "display this help")
+        ("add,a", po::value< string >(), "add a new entry to the To Do list")
+        ;
+
+    bool parseError = false;
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch (po::error& error)
+    {
+        cerr << "Error: " << error.what() << "\n" << endl;
+        parseError = true;
+    }
+
+    if (parseError || vm.count("help"))
+    {
+        cout << "todo:  A simple To Do list program" << "\n";
+        cout                                         << "\n";
+        cout << "Usage:"                             << "\n";
+        cout << "  " << argv[0] << " [options]"      << "\n";
+        cout                                         << "\n";
+        cout << desc                                 << "\n";
+
+        if (parseError)
+        {
+            return 64;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
 
     ToDo list;
 
@@ -32,41 +61,14 @@ int main(
     list.addTask("compile");
     list.addTask("test");
 
-    result |= EXPECT_EQUAL(list.size(),     size_t(3));
-    result |= EXPECT_EQUAL(list.getTask(0), "write code");
-    result |= EXPECT_EQUAL(list.getTask(1), "compile");
-    result |= EXPECT_EQUAL(list.getTask(2), "test");
-
-    if (result == 0)
+    if (vm.count("add"))
     {
-        cout << "Test passed" << endl;
+        list.addTask(vm["add"].as< string >());
     }
 
-    return result;
-}
-
-
-template < typename T1, typename T2 >
-int equalityTest(
-    const T1    testValue,
-    const T2    expectedValue,
-    const char* testName,
-    const char* expectedName,
-    const char* fileName,
-    const int   lineNumber
-)
-{
-    if (testValue != expectedValue)
+    for (size_t i = 0; i < list.size(); ++i)
     {
-        cerr << fileName << ":" << lineNumber << ": "
-             << "Expected " << testName << " "
-             << "to equal " << expectedName << " (" << expectedValue << ") "
-             << "but it was (" << testValue << ")" << endl;
-
-        return 1;
+        cout << list.getTask(i) << "\n";
     }
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
